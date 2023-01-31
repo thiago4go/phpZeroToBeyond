2aefda7b55c8426e4c31bd80c8b9f16eccd42c73
thiago4go <shimada+github@nikkei.one>
2023-01-31 10:03:41 +0900

    feat: new php file added

--- Concepts/Advanced/dateTime.php
+++ Concepts/Advanced/dateTime.php
@@ -4,4 +4,4 @@ echo "Today is" . date("d/m/y H:i") . "<br>";
 echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
+asdf;
